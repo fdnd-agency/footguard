4e0b1d9cc8e2054485b5ae538db4aa5f77e69553
Ravirkt <r_tjikhoeri@live.com>
2025-10-08 23:43:48 +0200

    feature: added status label partials #28

---
+++ src/lib/partials/FinishedLabel.svelte
@@ -0,0 +1,29 @@
+<script>
+
+    import finished from "$lib/assets/svg/finished-icon.svg";
+
+</script>
+
+
+
+<div class="finished-label paragraph">
+    <img src={finished} alt="" height="15" width="15">
+    Finished
+</div>
+
+
+<style>
+       .finished-label {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.5rem;
+        border: 1px solid var(--green-700);
+        width: fit-content;
+        padding: 0.35rem;
+        border-radius: 0.5rem;
+        color: var(--green-700);
+        background-color: var(--green-100);
+        white-space: nowrap;
+    }
+</style>
---
+++ src/lib/partials/NotStartedLabel.svelte
@@ -0,0 +1,29 @@
+<script>
+
+    import notStarted from "$lib/assets/svg/notstarted-icon.svg";
+
+</script>
+
+
+
+<div class="finished-label paragraph">
+    <img src={notStarted} alt="" height="15" width="15">
+    Not started
+</div>
+
+
+<style>
+       .finished-label {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.5rem;
+        border: 1px solid var(--red-400);
+        width: fit-content;
+        padding: 0.35rem;
+        border-radius: 0.5rem;
+        color: var(--red-400);
+        background-color: var(--red-100);
+        white-space: nowrap;
+    }
+</style>
