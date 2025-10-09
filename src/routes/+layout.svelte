2939f4d5f771b4e7cdcfbe9a4debe456ef3989c9
Ravirkt <r_tjikhoeri@live.com>
2025-10-09 20:15:50 +0200

    feature: added dynamic tab name in layout file and added tab icon #28

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script>
 	import '$lib/css/styleguide.css'
+	import logo from '$lib/assets/img/footguard-icon.webp'
 
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>Footguard</title>
+	<link rel="icon" href="{logo}">
 </svelte:head>
 
 {@render children?.()}
