03370a18ce5ce260c4a93443b8888b74a1d38b1e
Ravirkt <r_tjikhoeri@live.com>
2025-10-14 19:47:54 +0200

    feature: made component for not started filter button #28

---
+++ src/lib/partials/Not-started-button-filter.svelte
@@ -0,0 +1,37 @@
+<script>
+
+    import notStarted from "$lib/assets/svg/notstarted-icon.svg";
+
+</script>
+
+    <button class="filter-button not-started" type="submit" name="filter" value="notstarted">
+        <img src={notStarted} alt="" height="15" width="15">
+        Not started
+    </button>
+
+<style>
+
+    .filter-button {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.5rem;
+        white-space: nowrap;
+        padding: 0.5rem;
+        border-radius: 0.5rem;
+        width: fit-content;
+        cursor: pointer;
+
+        &:hover {
+            background-color: var(--red-200); 
+            color: var(--main-text-color-light);
+        }
+    }
+
+       .not-started {
+        border: 1px solid var(--red-400);
+        color: var(--red-400);
+        background-color: var(--red-100);
+    }
+
+</style>
