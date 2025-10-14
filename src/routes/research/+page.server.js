e61ac93506643ea6db62cbf1ca47687ad053cd69
Ravirkt <r_tjikhoeri@live.com>
2025-10-14 19:50:13 +0200

    feature: fetched data with mock api and implemented it in component #28

---
+++ src/routes/research/+page.server.js
@@ -0,0 +1,11 @@
+export async function load ({ url }) {
+    const gradingsResponse = await fetch('https://68ee3b05df2025af7802de69.mockapi.io/assigned/articles/Gradings');
+    const gradingsData = await gradingsResponse.json();
+    console.log(gradingsData)
+
+    let gradings = gradingsData.data
+
+    return {gradings: gradingsData}
+
+
+}
