f422596413f2d64a6d8e8baeb74a0e40604e1cdc
Ravirkt <r_tjikhoeri@live.com>
2025-10-16 15:26:11 +0200

    feature: fetch with mock api #43 #28

--- src/routes/research/+page.server.js
+++ src/routes/research/+page.server.js
@@ -1,3 +1,6 @@
+
+
+
 export async function load ({ url }) {
     const gradingsResponse = await fetch('https://68ee3b05df2025af7802de69.mockapi.io/assigned/articles/Gradings');
     const gradingsData = await gradingsResponse.json();
@@ -9,3 +12,5 @@ export async function load ({ url }) {
 
 
 }
+
+
