95090829f160543c78f1d2ace23180eeeed98255
Ravirkt <r_tjikhoeri@live.com>
2025-10-16 15:40:45 +0200

    feature: added card button with possibility to change button, href route and to add an new class text #28 #44

---
+++ src/lib/partials/Card-button.svelte
@@ -0,0 +1,24 @@
+<script>
+	// for custom button text
+	export let buttonContent = "buttontext";
+	// for custom href route
+	export let hrefRoute = "/";
+
+	// add class to component
+	let className = "";
+	export { className as class };
+</script>
+
+<a href="/" class="card-button paragraph {className}">{buttonContent}</a>
+
+<style>
+	.card-button {
+		background-color: var(--blue-700);
+		color: var(--main-text-color-light);
+		padding: 0.5rem;
+		border-radius: 1rem;
+		width: fit-content;
+		white-space: nowrap;
+		font-size: clamp(10px, 1.5vw, 16px);
+	}
+</style>
