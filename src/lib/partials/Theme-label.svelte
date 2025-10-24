52ff564da8c52cf3e35a2cea6bab734ce92f76ea
Ravirkt <r_tjikhoeri@live.com>
2025-10-24 14:33:02 +0200

    feature: added partial for theme label and added new filter button to filter theme #59

---
+++ src/lib/partials/Theme-label.svelte
@@ -0,0 +1,26 @@
+<script>
+
+    // add class to component
+    let className = '';
+    export { className as class };
+
+    export let themeName = '';
+</script>
+
+<span class="theme-label sub-text {className}">{themeName}</span>
+
+<style>
+    .theme-label {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.5rem;
+        border: 1px solid var(--blue-400);
+        width: fit-content;
+        padding: 0.25rem;
+        border-radius: 0.5rem;
+        color: var(--main-text-color-light);
+        background-color: var(--blue-500);
+        white-space: nowrap;
+    }
+</style>
