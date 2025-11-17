1df079ee557133755b39e5f3c73b48cf9ea81cc1
SuleymanHG <suleyman.gokgul66@outlook.com>
2025-11-17 15:07:33 +0100

    #101 Bug gefixt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  import favicon from "$lib/assets/favicon.svg";
+  // import favicon from "$lib/assets/favicon.svg";
   import styleguide from "$lib/css/styleguide.css";
 //   import variables from "$lib/css/variables.css";
   import logo from '$lib/assets/img/footguard-icon.webp'
