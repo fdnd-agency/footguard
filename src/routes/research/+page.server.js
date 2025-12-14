4273d965c78961db44cbb8368e35411e25721646
Ravirkt <r_tjikhoeri@live.com>
2025-12-14 19:37:35 +0100

    feat: added filtering code for them filtering and added directus api #28 #98

--- src/routes/research/+page.server.js
+++ src/routes/research/+page.server.js
@@ -1,16 +1,46 @@
 
+// https://fdnd-agency.directus.app/items/footguard_articles
 
+// https://68ee3b05df2025af7802de69.mockapi.io/assigned/articles/Gradings
 
 export async function load ({ url }) {
-    const gradingsResponse = await fetch('https://68ee3b05df2025af7802de69.mockapi.io/assigned/articles/Gradings');
+  // Dit leest de url af op de filter waarde is de filterwaarde geen morning, evening of is het null, dan pakt hij alle
+
+    const filter = url.searchParams.get('theme') || 'all';
+  console.log(filter)
+
+
+
+    const gradingsResponse = await fetch('https://fdnd-agency.directus.app/items/footguard_articles');
     const gradingsData = await gradingsResponse.json();
     console.log(gradingsData)
 
-    let gradings = gradingsData.data
+    let cardData = gradingsData.data
 
-    return {gradings: gradingsData}
+
+    
+    if (filter === 'Temperature') {
+        cardData = cardData.filter(cardData => cardData.theme === "Temperature");
+    } else if (filter === 'Ucles') {
+        cardData = cardData.filter(cardData => cardData.theme === "Ucles");
+    } else if (filter === 'High-risk') {
+        cardData = cardData.filter(cardData => cardData.theme === "High-risk");
+    } else if  (filter === 'Age') {
+        cardData = cardData.filter(cardData => cardData.theme === "Age");
+    }
+
+    // return {gradings: gradingsData.data}
+
+    return {cardData, filter}
 
 
 }
+
+
+
+
+
+
+
 
 
