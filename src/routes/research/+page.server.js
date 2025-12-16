a74151c74b4c0ae486a6abebd9206e1123b9caa4
Ravirkt <r_tjikhoeri@live.com>
2025-12-16 15:10:20 +0100

    feat: added filter functionality and added trim and simple comments #98 #28

--- src/routes/research/+page.server.js
+++ src/routes/research/+page.server.js
@@ -1,25 +1,10 @@
 
 // https://fdnd-agency.directus.app/items/footguard_articles
-
 // https://68ee3b05df2025af7802de69.mockapi.io/assigned/articles/Gradings
-
-// export async function load ({ url }) {
-
-//     const gradingsResponse = await fetch('https://fdnd-agency.directus.app/items/footguard_articles');
-//     const gradingsData = await gradingsResponse.json();
-//     console.log(gradingsData)
-
-
-//   
-
-//   return {gradings: gradingsData.data}
-
-
-// }
 
 
 export async function load ({ url }) {
-  // Dit leest de url af op de filter waarde is de filterwaarde geen morning, evening of is het null, dan pakt hij alle
+  // Dit leest de url af op de filter waarde is de filterwaarde voor theme of status niet ingevuld dan pakt het alle
 
     const status = url.searchParams.get('status') || 'all';
     const theme = url.searchParams.get('theme') || 'all';
@@ -34,32 +19,29 @@ export async function load ({ url }) {
     let cardData = gradingsData.data
 
 
-    
+    // de status filteren op betreffende waarde uit de url
     if (status === 'Not started') {
-        cardData = cardData.filter(cardData => cardData.status === "Not started");
-    } else if (status === 'Finished') {
-        cardData = cardData.filter(cardData => cardData.status === "Finished");
-    } else if (status === 'In progress') {
-        cardData = cardData.filter(cardData => cardData.status === "In progress");
-    } 
+            cardData = cardData.filter(cardData => cardData.status === "Not started");
+        } else if (status === 'Finished') {
+            cardData = cardData.filter(cardData => cardData.status === "Finished");
+        } else if (status === 'In progress') {
+            cardData = cardData.filter(cardData => cardData.status === "In progress");
+        } 
 
-if (theme === 'Temperature') {
-        cardData = cardData.filter(cardData => cardData.theme === "Temperature");
-    } else if (theme === 'Ulcers') {
-        cardData = cardData.filter(cardData => cardData.theme === "Ulcers");
-    } else if (theme === 'High risk ') {
-        cardData = cardData.filter(cardData => cardData.theme === "High risk ");
-    } else if  (theme === 'Age') {
-        cardData = cardData.filter(cardData => cardData.theme === "Age");
-    }
+    // De theme filteren op betreffende waarde uit de url
+    if (theme === 'Temperature') {
+            cardData = cardData.filter(cardData => cardData.theme === "Temperature");
+        } else if (theme === 'Ulcers') {
+            cardData = cardData.filter(cardData => cardData.theme === "Ulcers");
+        } else if (theme === 'High risk') {
+            cardData = cardData.filter(cardData => cardData.theme.trim() === "High risk");
+        } else if  (theme === 'Age') {
+            cardData = cardData.filter(cardData => cardData.theme === "Age");
+        }
 
-
-    // return {gradings: gradingsData.data}
+        // return {gradings: gradingsData.data}
 
     return {cardData, status, theme}
-
-
-
 
 }
 
