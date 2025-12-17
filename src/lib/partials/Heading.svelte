540105e3bca8bde7e70f4c184b7fe6b0ea346681
Ravirkt <r_tjikhoeri@live.com>
2025-12-18 00:12:08 +0100

    feat: added heading component #60

---
+++ src/lib/partials/Heading.svelte
@@ -0,0 +1,23 @@
+<script>
+
+  let {title, subTitle, className} = $props()
+
+</script>
+
+<div class="page-title">
+    <h1>{title}</h1>
+    <p>{subTitle}</p>
+
+</div>
+
+<style>
+
+    .page-title {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        padding: 1.5rem 0 2rem 0;
+    }
+
+
+</style>
