8dccf0d3c79a21d2bbb4cabb89813ce28bcf6428
Ravirkt <r_tjikhoeri@live.com>
2026-01-15 19:11:38 +0100

    fix: removed unused components #60 #192 #191

---
+++ src/lib/components/cards/ResearchArticleCard.svelte
@@ -0,0 +1,131 @@
+<script>
+	// components
+	import InProgressLabel from "$lib/components/textual/InProgressLabel.svelte";
+	import FinishedLabel from "$lib/components/textual/FinishedLabel.svelte";
+	import NotStartedLabel from "$lib/components/textual/NotStartedLabel.svelte";
+	import ThemeLabel from "$lib/components/textual/ThemeLabel.svelte";
+
+	// icons
+	import authorIcon from "$lib/assets/svg/author-icon.svg";
+	import calenderIcon from "$lib/assets/svg/calendar-icon.svg";
+
+	// Dynamic data variables
+	let {name, article_id, Publisher, publishing_year, status, className, theme} = $props()
+
+</script>
+
+<a class="anchor-container-card" href="/research/{article_id}">
+	<article class="research-card">
+		<div class="research-card-data-container">
+			<h2 class="research-card-title h4">
+				<span class="research-id">{article_id} # -</span>
+				{name}
+			</h2>
+
+			<div class="research-card-author-date-container">
+				<figure class="author-container">
+					<img src="{authorIcon}" class="author-icon" alt="" height="15" width="15">
+					<figcaption class="author-name">{Publisher}</figcaption>
+				</figure>
+
+				<figure class="calender-container">
+					<img src="{calenderIcon}" class="calender-icon" alt="" height="15" width="15">
+					<time class="calender-date">{publishing_year}</time>
+				</figure>
+			</div>
+
+			<div class="action-container">
+
+				{#if status === "Not started"}
+					<NotStartedLabel/>
+				{:else if status === "In progress"}
+					<InProgressLabel/>
+				{:else if status === "Finished"}
+					<FinishedLabel/>
+				{/if}
+
+                <ThemeLabel themeName={theme} />
+
+			</div>
+		</div>
+	</article>
+</a>
+
+<style>
+	.anchor-container-card {
+		transition: transform 0.3s ease;
+
+		&:hover {
+			transform: translateY(-2px);
+		}
+	}
+
+	.research-card {
+		padding: 1rem;
+		border-radius: 1rem;
+		background-color: hsla(197, 7%, 79%, 0.127);
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+
+		&:hover {
+			background-color: hsl(200, 90%, 96%);
+		}
+
+		@media (min-width: 700px) {
+			flex-direction: row;
+			align-items: center;
+		}
+	}
+
+	.research-card-data-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 0.5rem;
+
+		& .research-card-title {
+			font-weight: bold;
+			color: var(--grey-700);
+
+			display: -webkit-box;
+			-webkit-line-clamp: 2; 
+			-webkit-box-orient: vertical;
+			overflow: hidden;
+		}
+
+		& h2 .research-id {
+			color: var(--grey-500);
+		}
+	}
+
+	.research-card-author-date-container {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 0.5rem;
+
+		& .author-container,
+		.calender-container {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			gap: 0.2rem;
+			justify-content: center;
+			text-align: center;
+		}
+
+		& .author-name,
+		.calender-date {
+			font-size: clamp(10px, 1.5vw, 12px);
+			color: var(--grey-700);
+		}
+	}
+
+	.action-container {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 0.5rem;
+	}
+</style>
