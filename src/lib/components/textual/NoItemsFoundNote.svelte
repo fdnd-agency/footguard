fdf2df7db7bfdd7ce7241513793dbad6a39437cc
Ravirkt <r_tjikhoeri@live.com>
2026-01-20 18:42:54 +0100

    feature: component note filtering #191

---
+++ src/lib/components/textual/NoItemsFoundNote.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { fly } from "svelte/transition";
+</script>
+
+
+<p class="no-results-text" transition:fly={{ y:600, duration:700 }}>No items found</p>
+
+<style>
+	.no-results-text {
+		padding: 4rem;
+		text-align: center;
+		background-color: var(--grey-100);
+		margin-top: 2rem;
+		border-radius: 1rem;
+		color: var(--grey-700);
+	}
+
+</style>
