a69d97785b2da8557058d620a1590384cc24ea68
Yamen <62349907+yamenAl@users.noreply.github.com>
2026-02-09 15:24:50 +0100

    fix: add fetch parameter and remove console.log #244
    - Add `fetch` parameter to load function
    
    - Remove unnecessary console.log statement

--- src/routes/research/[article_id]/+page.server.js
+++ src/routes/research/[article_id]/+page.server.js
@@ -1,11 +1,10 @@
-export async function load({ params }) {
+export async function load({ fetch, params }) {
   const articleId = params.article_id
-
+  
   const detailsResponse = await fetch(
     'https://fdnd-agency.directus.app/items/footguard_articles/' + articleId
   )
   const detailsData = await detailsResponse.json()
-  console.log(detailsData)
 
   let detailsInfo = detailsData.data
 
@@ -13,7 +12,6 @@ export async function load({ params }) {
     'https://fdnd-agency.directus.app/items/footguard_checklists'
   )
   const questionData = await questionResponse.json()
-  console.log(questionData)
 
   let questionInfo = questionData.data
 
