d1bf1554c7aa6deeaf9b11c6694dd7faa4db5f43
Yamen <62349907+yamenAl@users.noreply.github.com>
2026-03-09 10:13:10 +0100

    removing title from the profile page #275
    Removing the user title from the profile page

--- src/lib/components/profile/ProfileInfo.svelte
+++ src/lib/components/profile/ProfileInfo.svelte
@@ -7,11 +7,7 @@
 
 <section>
   <h2>General Information</h2>
-  <p>
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-    the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-    of type and scrambled it to make a type specimen book.
-  </p>
+  
 
   <form class="info-grid" aria-label="Profile details">
     <label for="info-role">
