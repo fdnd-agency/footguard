9ddfe913a48dc3781f8440725ff3933618f346d4
Yamen <62349907+yamenAl@users.noreply.github.com>
2026-03-12 00:21:10 +0100

    feat: add UserSectionDropdown component #265
    Adds a dropdown section listing group members with avatar.

---
+++ src/lib/components/groups/UserSectionDropdown.svelte
@@ -0,0 +1,24 @@
+<script>
+  import avatar from '$lib/assets/img/profile-avatar.webp';
+</script>
+
+<section aria-label="Group members details">
+  <ul>
+    <li>
+      <img src={avatar} alt="User 1 name" />
+      <span>User 1 name</span>
+      <form method="POST" action="?/remove">
+        <input type="hidden" name="memberId" value="1" />
+        <button type="submit">Remove</button>
+      </form>
+    </li>
+    <li>
+      <img src={avatar} alt="User 2 name" />
+      <span>User 2 name</span>
+      <form method="POST" action="?/remove">
+        <input type="hidden" name="memberId" value="2" />
+        <button type="submit">Remove</button>
+      </form>
+    </li>
+  </ul>
+</section>
