2b10c00f803318516d17c92c1062ad0ee3235c25
Yamen <62349907+yamenAl@users.noreply.github.com>
2026-05-11 13:13:05 +0200

    feat: add PlusIcon SVG component #367
    Add a reusable plus SVG icon as svelte file

---
+++ src/lib/assets/svg/puls-icon-no-background.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let width = 17;
+  export let height = 17;
+</script>
+
+<svg
+  {width}
+  {height}
+  viewBox="0 0 17 17"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <path
+    d="M8.25 1.25V15.25M1.25 8.25H15.25"
+    stroke="currentColor"
+    stroke-width="2.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
