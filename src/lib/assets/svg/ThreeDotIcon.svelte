97fc4a6caaca76d177bf99550966b281ae0e76e5
Yamen <62349907+yamenAl@users.noreply.github.com>
2026-06-02 23:28:18 +0200

    feat: add ThreeDotsIcon SVG component #398

---
+++ src/lib/assets/svg/ThreeDotIcon.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let width = 16
+  export let height = 16
+</script>
+
+<svg
+  {width}
+  {height}
+  viewBox="0 0 11 3"
+  fill="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <circle cx="1.44737" cy="1.44737" r="1.44737" />
+  <circle cx="5.5001" cy="1.44737" r="1.44737" />
+  <circle cx="9.55284" cy="1.44737" r="1.44737" />
+</svg>
